79399dbf79cd977886b12fe96c2f9593ccbe4a4b
Ana Paula Nunes Cerchiari Almeida <aalmeida2014@kellogg.northwestern.edu>
2013-10-28 21:05:42 -0500

    Latitude and Longitude
    Showing the address latitude and longitude

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,10 +7,19 @@ class AddressesController < ApplicationController
     @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @latitude = ?
-    # @longitude = ?
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Foster+and+Sheridan,+Evanston,+IL&sensor=false"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+
+    results = parsed_data["results"]
+    first = results[0]
+    first.keys
+
+    geometry = first["geometry"]
+    geometry.keys
+    location = geometry["location"]
+
+    @latitude = location["lat"]
+    @longitude = location["lng"]
   end
 end
